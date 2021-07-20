f2b9b6b400f1ef7d4f2d02c9f9be093c8c5c8f18
Juan Torres <jtorres@noven.io>
2021-07-20 16:19:56 -0500

    Basic redux

---
+++ app-2.ts
@@ -0,0 +1,15 @@
+import {
+    decrementadorAction,
+    dividirAction,
+    incrementadorAction,
+    multiplicarAction,
+    resetAction
+} from "./contador/contador.actions";
+
+import { contadorReducer } from "./contador/contador.reducer";
+
+console.log(contadorReducer(10, incrementadorAction));
+console.log(contadorReducer(10, decrementadorAction));
+console.log(contadorReducer(10, multiplicarAction));
+console.log(contadorReducer(10, dividirAction));
+console.log(contadorReducer(10, resetAction));
